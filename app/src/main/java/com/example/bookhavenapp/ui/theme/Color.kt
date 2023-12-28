52e9a65537a7499f85e7b45e9fd0d1c7635675a0
lizanette <a_lizacortes10@outlook.com>
2023-12-27 20:55:22 -0600

    Initial commit

---
+++ app/src/main/java/com/example/bookhavenapp/ui/theme/Color.kt
@@ -0,0 +1,21 @@
+package com.example.bookhavenapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PurpleGrey40 = Color(0xFF625b71)
+
+// Backgrounds
+val Beige = Color(0xFFF0EBE0)
+val DarkGrey = Color(0xFF1C1E21)
+val LightBlack = Color(0xFF3A3B3C)
+
+// Primary
+val Blue = Color(0xFF5FA8D3)
+
+// Errors
+val DarkRed = Color(0xFFC30052)
+val LightRed = Color(0xFFFF84B7)
+
+val BlueGray = Color(0xFFA0A3BD)
+val WhiteGray = Color(0xFFB0B3B8)
+val Purple = Color(0xFFB47EB3)
